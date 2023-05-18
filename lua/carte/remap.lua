88618f5be3c91c067471ba087bdac801a66bfb34
Carte <7457451+corycarte@users.noreply.github.com>
2023-05-18 13:22:14 -0400

    Add window remaps

--- lua/carte/remap.lua
+++ lua/carte/remap.lua
@@ -9,8 +9,23 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
+-- Let me save normally
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
+
+-- Window movements
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+
 
 -- Visual Mode Remaps
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Remaps in after for specific plugins
+-- fugitive.lua
+-- lsp.lua
+-- neotree.lua
+-- telescope.lua
+-- trouble.lua
