f30968d1dc9f98fecd5d7c773f6571a91dea76c8
Carte <7457451+corycarte@users.noreply.github.com>
2023-05-25 13:42:55 -0400

    Update comments configuration

--- after/plugin/comments.lua
+++ after/plugin/comments.lua
@@ -1,16 +1,18 @@
+-- Available Comment Types
+-- FIX:  Example
+-- TODO: Example
+-- HACK: Example
+-- WARN: Example
+-- PERF: Example
+-- NOTE: Example
+-- TEST: Example
+-- PASSED: Example
+-- FAILED: Example
+
 require("todo-comments").setup {
-    keywords = {
-        TODO = { icon = " ", color = "info" },
-        HACK = { icon = " ", color = "warning" },
-        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-    },
-    colors = {
-        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-        info = { "DiagnosticInfo", "#2563EB" },
-        hint = { "DiagnosticHint", "#10B981" },
-        default = { "Identifier", "#7C3AED" },
-        test = { "Identifier", "#FF00FF" },
+    highlight = {
+        before = "bg",
+        after = "bg"
     }
 }
 
