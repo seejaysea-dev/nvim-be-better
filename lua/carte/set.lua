fe64a3165c7db3076444cb468498dffc913ff6a5
Carte <7457451+corycarte@users.noreply.github.com>
2024-08-19 12:15:00 -0400

    Update tabstop in set.lua

--- lua/carte/set.lua
+++ lua/carte/set.lua
@@ -4,9 +4,9 @@ vim.opt.cursorline = true
 
 vim.opt.ignorecase = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -15,7 +15,7 @@ vim.opt.wrap = false
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 12
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
