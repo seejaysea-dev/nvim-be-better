971c93bd1e95cd5ec01b41b99e2eb72194dea07f
Carte <7457451+corycarte@users.noreply.github.com>
2024-12-12 01:12:47 -0500

    add mini.diff plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -57,4 +57,27 @@ return {
       end,
     },
   },
+  {
+    "echasnovski/mini.diff",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>go",
+        function()
+          require("mini.diff").toggle_overlay(0)
+        end,
+        desc = "Toggle mini.diff overlay",
+      },
+    },
+    opts = {
+      view = {
+        style = "sign",
+        signs = {
+          add = "▎",
+          change = "▎",
+          delete = "",
+        },
+      },
+    },
+  },
 }
