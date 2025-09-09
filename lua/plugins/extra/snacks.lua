9ab4acebe3ffa6395c135ddbced8415f40a4344b
Carte <7457451+seejaysea-dev@users.noreply.github.com>
2025-09-09 15:15:09 -0400

    Add TODOs for snacks

--- lua/plugins/extra/snacks.lua
+++ lua/plugins/extra/snacks.lua
@@ -6,11 +6,14 @@ return {
     ---@type snacks.Config
     opts = {
       animate = { enabled = false },
-      bigfile = { enabled = false },
+      -- TODO: Confirm bigfile settings
+      bigfile = { enabled = true },
       dim = { enabled = false },
       notifier = { enabled = true },
       util = { enabled = false },
+      indent = { enabled = true },
     },
+    -- TODO: Setup keys
     keys = {
     },
   }
