b72e3bda0764bab8643df506b456e6453ad74df9
Carte <7457451+seejaysea-dev@users.noreply.github.com>
2026-04-14 06:20:49 -0400

    Update noice config

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -3,8 +3,7 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
+      { "MunifTanjim/nui.nvim" },
     },
     opts = {
       lsp = {
@@ -12,6 +11,25 @@ return {
       },
       cmdline = {
         view = "cmdline_popup",
+      },
+      messages = {
+        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+        -- This is a current Neovim limitation.
+        enabled = true, -- enables the Noice messages UI
+        view = "mini", -- default view for messages
+        view_error = "notify", -- view for errors
+        view_warn = "notify", -- view for warnings
+        view_history = "messages", -- view for :messages
+        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      },
+      notify = {
+        -- Noice can be used as `vim.notify` so you can route any notification like other messages
+        -- Notification messages have their level and other properties set.
+        -- event is always "notify" and kind can be any log level as a string
+        -- The default routes will forward notifications to nvim-notify
+        -- Benefit of using Noice for this is the routing and consistent history view
+        enabled = true,
+        view = "notify",
       },
       routes = {
         {
@@ -45,29 +63,22 @@ return {
         function()
           require("noice").cmd("history")
         end,
-        desc = "Noice: Message History"
+        desc = "Noice: Message History",
       },
       {
         "<leader>sna",
         function()
           require("noice").cmd("all")
         end,
-        desc = "Noice: All Messages"
+        desc = "Noice: All Messages",
       },
       {
         "<leader>snd",
         function()
           require("noice").cmd("dismiss")
         end,
-        desc = "Noice: Dismiss All"
+        desc = "Noice: Dismiss All",
       },
     },
-    config = function(_, opts)
-      if vim.o.filetype == "lazy" then
-        vim.cmd([[messages clear]])
-      end
-
-      require("noice").setup(opts)
-    end,
   },
 }
